bae8828525c5379f3667f5005c365c376075d85a
Daniel Curilla <curilladaniel@gmail.com>
2021-02-17 14:12:55 -0700

    Create player factory

--- script.js
+++ script.js
@@ -9,11 +9,23 @@ function createGrid(m,n){
     return arr;
 }
 
-Board = (function(){
-    let _grid = createGrid(3,3);
-    const getGrid = () => {return _grid};
-    const isEmpty = (row,col) => {return _grid[row][col] === null};
-    const setMarker = (symbol,row,col) => (_grid[row][col] = symbol);
-    const reset = () => (_grid = createGrid(3,3));
-    return {getGrid, isEmpty, setMarker, reset}
+function createPlayer(symbol){
+    return {symbol}
+}
+
+
+
+Game = (function(){
+
+    Board = (function(){
+        let _grid = createGrid(3,3);
+        const getGrid = () => {return _grid};
+        const isEmpty = (row,col) => {return _grid[row][col] === null};
+        const setMarker = (symbol,row,col) => (_grid[row][col] = symbol);
+        const reset = () => (_grid = createGrid(3,3));
+        return {getGrid, isEmpty, setMarker, reset}
+    }())
+
+    const p1 = createPlayer('X');
+    const p2 = createPlayer('O');
 }())
