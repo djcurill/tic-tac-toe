1fd9f1092b0c68a85a4eb30240498fbe6a5dc57b
Daniel Curilla <curilladaniel@gmail.com>
2021-02-18 13:59:08 -0700

    Move Board outside of Game scope

--- script.js
+++ script.js
@@ -21,27 +21,24 @@ function createPlayer(symbol,name){
     return {getSymbol,getName, addPoint, getScore}
 }
 
+Board = (function(){
+    let _grid = createGrid(3,3);
+    const getGrid = () => {return _grid};
+    const isEmpty = (row,col) => {return _grid[row][col] === null};
+    const setMarker = (player,row,col) => (_grid[row][col] = player.getSymbol());
+    const reset = () => (_grid = createGrid(3,3));
+    const display = (cells) => (
+        cells.forEach(cell => 
+            cell.textContent = _grid[getRow(cell)][getCol(cell)])
+    )
+    return {getGrid, isEmpty, setMarker, reset, display}
+}())
+
 
 Game = (function(){
-
-    Board = (function(){
-        let _grid = createGrid(3,3);
-        const getGrid = () => {return _grid};
-        const isEmpty = (row,col) => {return _grid[row][col] === null};
-        const setMarker = (player,row,col) => (_grid[row][col] = player.getSymbol());
-        const reset = () => (_grid = createGrid(3,3));
-        const display = () => (
-            cells.forEach(cell => 
-                cell.textContent = _grid[getRow(cell)][getCol(cell)])
-        )
-        return {getGrid, isEmpty, setMarker, reset, display}
-    }())
-
     let p1, p2;
     let currentPlayer;
     let cells = document.querySelectorAll("div.cell");
-
-
 
     const _checkRows = (i) =>  Board.getGrid()[i]
                                     .every(x => x === currentPlayer.getSymbol())
@@ -57,6 +54,10 @@ Game = (function(){
         return diag1 || diag2;
     }
 
+    const _setScore = function(player){
+        document.getElementById(player.getName()).textContent = `${player.getName()} : ${player.getScore()}`
+    }
+
     const checkWinner = function(){
         if (_checkDiags()) {return true}
         for (let i=0; i < 3; i++){
@@ -70,7 +71,7 @@ Game = (function(){
         let col = getCol(event.target);
         if (Board.isEmpty(row,col)){
             Board.setMarker(currentPlayer,row,col);
-            Board.display();
+            Board.display(cells);
             if (checkWinner()){
                 currentPlayer.addPoint();
                 _setScore(currentPlayer);
@@ -80,9 +81,7 @@ Game = (function(){
         }
     }
 
-    const _setScore = function(player){
-        document.getElementById(player.getName()).textContent = `${player.getName()} : ${player.getScore()}`
-    }
+
 
     const initGame = function(event){
         event.preventDefault();
@@ -103,7 +102,7 @@ Game = (function(){
 
     const newRound = function(event){
         Board.reset();
-        Board.display();
+        Board.display(cells);
         document.getElementById("new-game").classList.toggle("hidden");
     }
 
