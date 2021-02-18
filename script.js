5da1b89b897f0ea0492ec5ad602617c6b6cba327
Daniel Curilla <curilladaniel@gmail.com>
2021-02-18 08:54:23 -0700

    Add winner logic to Game

--- script.js
+++ script.js
@@ -39,12 +39,39 @@ Game = (function(){
     let currentPlayer = p1;
     let cells = document.querySelectorAll("div.cell");
 
+
+
+    const _checkRows = (i) =>  Board.getGrid()[i]
+                                    .every(x => x === currentPlayer.getSymbol())
+    const _checkCols = (i) => Board.getGrid()
+                                    .map(x => x[i])
+                                    .every(x => x === currentPlayer.getSymbol())
+    const _checkDiags = function(){
+        let arr = Board.getGrid();
+        let diag1 = [arr[0][0],arr[1][1],arr[2][2]]
+                        .every(x => x === currentPlayer.getSymbol());
+        let diag2 = [arr[0][2],arr[1][1],arr[2][0]]
+                        .every(x => x === currentPlayer.getSymbol());
+        return diag1 || diag2;
+    }
+
+    const checkWinner = function(){
+        if (_checkDiags()) {return true}
+        for (let i=0; i < 3; i++){
+            if (_checkRows(i) || _checkCols(i)){return true}
+        }
+        return false
+    }
+
     const setMove = function(event){
         let row = getRow(event.target);
         let col = getCol(event.target);
         if (Board.isEmpty(row,col)){
             Board.setMarker(currentPlayer,row,col);
             Board.display();
+            if (checkWinner()){
+                console.log("Current player is the winner");
+            }
             currentPlayer = (currentPlayer.getSymbol() === 'X') ? p2 : p1
         }
     }
